16093b71caff211da2c7874503951fb259986a22
Lerdonardo <91110442+Lerdonardo@users.noreply.github.com>
2022-10-07 12:41:10 -0300

    Update and rename Pandas-JSON.py to Final Version.py

--- Final Version.py
+++ Final Version.py
@@ -1,9 +1,12 @@
 #STATUS
 #Conversão para dict-Completo.
-#Inclusão do JSON - 70%
+#Inclusão do JSON - 100%
 #Inclusão do Pandas-Completo
 
+
+#Início- Imports e variáveis declaradas
 import json
+import requests
 import pandas as pd
 alunos = []
 datahandle= {}
@@ -13,10 +16,11 @@ alunosrep= []
 médiase=[]
 nomes=[]
 arquivo='info.xlsx'
-classe={}
-r1=input('Qual classe sera atualizada/criada? ')
+classe={} 
+c=0
+#FIM- Imports e variáveis declaradas
 
-r2=int(input('A tabela excel será atualizada ou criada? (0 ou 1 respectivamente) '))
+#Início- Funções do manuseio das informações
 def dhi(nome):
     datahandle["Nome"]=nome
     datahandle["NP1"]=0
@@ -26,40 +30,43 @@ def dhi(nome):
     datahandle["Média"]=0
     datahandle["Situação"]="Nulo"
     alunos.append(datahandle.copy())
-def dhap(média,nota1,nota2,i,r4):
-    datahandle["Situação"]="APROVADO"
-    datahandle["Média"]=média
+
+def dh(c,i,nota1,nota2,média,notae):
+    r4=0
+    if c == 1:
+        r4="NÃO"
+        notae="Nulo"
+        stc="APROVADO"     
+    elif c == 2:
+        r4="SIM"
+        notae="Nulo"
+        stc="Nulo"
+    elif c == 3:
+        r4="SIM"
+        nota1=i["NP1"]
+        nota2=i["NP2"]
+        stc="APROVADO"
+    elif c == 4:
+        r4="SIM"
+        nota1=i["NP1"]
+        nota2=i["NP2"]
+        stc="REPROVADO" 
     datahandle["Nome"]=i["Nome"]
-    datahandle["Exame"]=r4
     datahandle["NP1"]=nota1
     datahandle["NP2"]=nota2
-    alunosap.append(datahandle.copy())
-def dhe(média,nota1,nota2,i,r4):
-    datahandle["Situação"]="Nulo"
-    datahandle["Média"]=média
-    datahandle["Nome"]=i["Nome"]
     datahandle["Exame"]=r4
-    datahandle["NP1"]=nota1
-    datahandle["NP2"]=nota2
-    alunose.append(datahandle.copy())
-    médiase.append(média)
-def dhrep(média,notae,i):
-    datahandle["Situação"]="REPROVADO"
-    datahandle["Média"]=média
     datahandle["NPE"]=notae
-    datahandle["NP1"]=i["NP1"]
-    datahandle["NP2"]=i["NP2"]
-    datahandle["Nome"]=i["Nome"]
-    datahandle["Exame"]="SIM"
-    alunosrep.append(datahandle.copy())
-    
+    datahandle["Média"]=média
+    datahandle["Situação"]=stc
+#FIM- Funções do manuseio das informações
+
+#Código principal
+r1=input('Qual classe sera atualizada/criada? ')
+r2=int(input('A tabela excel será atualizada ou criada? (0 ou 1 respectivamente) '))
 if r2==0:
     arquivo=input('Digite o nome do arquivo. (Não esqueça do sufixo, ".xlsx") ')
     tabela_df=pd.read_excel(arquivo)
-    for i in tabela_df["Nome"]:
-        nomes.append(i)
-    print(nomes)
-    for nome in nomes:          
+    for nome in tabela_df["Nome"]:
         dhi(nome)
     #print(alunos)
 r3=input('Algum novo aluno será adicionado? (s ou n repectivamente) ')
@@ -78,17 +85,21 @@ for i in alunos:
     total=0
     média=0
     n = 0
+    notae=0
     print(f'Coloque as notas do {i["Nome"]}:')
     nota1 = float(input())
     nota2 = float(input())
     média = (nota1+nota2)/2
     if média >=7.0:
-        r4="NÃO"
-        dhap(média,nota1,nota2,i,r4)
+        c=1
+        dh(c,i,nota1,nota2,média,notae)
+        alunosap.append(datahandle.copy())
         #print(f'{datahandle["Nome"]},{datahandle["Média"]}, APROVADO')
     else:
-        r4="SIM"
-        dhe(média,nota1,nota2,i,r4)
+        c=2
+        dh(c,i,nota1,nota2,média,notae)
+        alunose.append(datahandle.copy())
+        médiase.append(média)
         #print(f'{datahandle["Nome"]},{datahandle["Média"]}, EXAME')
 print('CÁLCULO EXAME')
 nn=-1
@@ -97,28 +108,31 @@ for i in alunose:
     notae = float(input(f'Nota do Exame do {i["Nome"]}: '))
     média = (médiase[nn] + notae)/2
     if média >=5.0:
-        datahandle["Situação"]="APROVADO"
-        datahandle["Média"]=média
-        datahandle["NPE"]=notae
-        datahandle["NP1"]=i["NP1"]
-        datahandle["NP2"]=i["NP2"]
-        datahandle["Nome"]=i["Nome"]
-        datahandle["Exame"]="SIM"
+        c=3
+        dh(c,i,nota1,nota2,média,notae)
         alunosap.append(datahandle.copy())
         #print(f'{datahandle["Nome"]},{datahandle["Média"]}, {datahandle["Situação"]}')
     else:
-        dhrep(média,notae,i)
+        c=4
+        dh(c,i,nota1,nota2,média,notae)
+        alunosrep.append(datahandle.copy())
         #print(f'{datahandle["Nome"]},{datahandle["Média"]}, {datahandle["Situação"]}')
-        
-                
+#FIM- Código Principal
+
+#Início-Criação/atualização da tabela
 tabelaap=pd.DataFrame(alunosap)
 tabelarep=pd.DataFrame(alunosrep)
 tabela_df=pd.concat([tabelaap,tabelarep], ignore_index=True)
 print(tabela_df)
 tabela_df.to_excel(arquivo)
+#FIM-Criação/atualização da tabela
+
+#Início-Criação/atualização da api
+r5=input('Qual o link da api')
 classe[r1]=alunosap,alunosrep
 api=json.dumps(classe)
-print(api)
+requests.post(r5,api)
+#FIM-Criação/atualização da api
 
 print('RESUMO')
 print()
